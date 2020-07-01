5215d1458d2bd0d44f7b3a154ab1be5f90c4de72
Erik Buunk <erik_buunk@iq.harvard.edu>
2020-07-01 14:26:05 -0400

    Update date for business operations and Homepage assignment

--- business_operations.py
+++ business_operations.py
@@ -5,6 +5,8 @@ import logging
 import csv
 from datetime import date
 import pandas as pd
+from metrics import write_metric
+
 
 def harvest_business_operations(path):
     """Harvest Business Operations Spreadsheet"""
@@ -14,16 +16,30 @@ def harvest_business_operations(path):
     harvest_sheet_tsv(path=path,
                       name="business_operations",
                       sheet_id='1TXGcNBAPYmAgITwJ7dBAP2cB3Kkfigss_Kcr2A6jwUE',
-                      range_name='Sponsored Research!A:G',
-                      columns=[0, 1, 2, 3, 4, 5, 6])
+                      range_name='Sponsored Research!A:I',
+                      columns=[])
 
-def aggregate_bo (path, tsv):
+
+def aggregate_bo(path, tsv):
     df = pd.read_csv(path + tsv + ".tsv", delimiter="\t")
-    df1 = df[df["Group"] == "Sponsored Research Administration"]
-    df2 = df[df["Group"] != "Sponsored Research Administration"]
+    df1 = df[df["Group"] == "Sponsored Research Administration"].iloc[:, 0:7]
+    df2 = df[df["Group"] != "Sponsored Research Administration"].iloc[:, 0:7]
+
+    update_date = df[df["Updated"].isnull() == False]["Updated"][0]
+    pd.DataFrame({"Department": ["Business Operations"], "Last Update": update_date}).to_csv(path + "update_dates.tsv",
+                                                                                             sep='\t', index=False)
+
+    metric_df = df[df["Homepage"].isnull() == False].values.tolist()[0]
 
     df1.to_csv(path + "sponsored_research_administration.tsv", sep='\t', index=False)
     df2.to_csv(path + "finance_and_administration.tsv", sep='\t', index=False)
+
+    write_metric(path=path, group=metric_df[0], metric=metric_df[1],
+                 title=metric_df[1],
+                 value=metric_df[6], unit=metric_df[5],
+                 icon=metric_df[2], color=metric_df[4],
+                 url=metric_df[3])
+
 
 def harvest_social_media(path):
     logging.info("Harvesting Social Media")
