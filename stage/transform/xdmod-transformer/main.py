5f25807facd2876ca59797b7e2b85823cfb1e40b
Evan Sarmiento <evansarm@gmail.com>
2025-05-22 11:05:17 -0400

    Add and implement IQ map

--- stage/transform/xdmod-transformer/main.py
+++ stage/transform/xdmod-transformer/main.py
@@ -8,12 +8,13 @@ from pathlib import Path
 import pandas as pd
 import click
 
-MAP_FILE = "./_data/pi_2024-10-16T21.56.12.csv"
+FASRC_MAP_FILE = "./_data/pi_2024-10-16T21.56.12.csv"
+IQSS_MAP_FILE = "./_data/iq_2025.csv"
 
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.argument(
-    "input_csv",
+    "by_user_csv",
     type=click.Path(file_okay=True, dir_okay=False, path_type=Path),
 )
 @click.argument(
@@ -21,19 +22,41 @@ MAP_FILE = "./_data/pi_2024-10-16T21.56.12.csv"
     type=click.Path(file_okay=True, dir_okay=False, path_type=Path),
     default="./__output__/out.tsv",
 )
-def cli(input_csv: Path, output_csv: Path) -> None:
+@click.option(
+    "--by_pi_csv",
+    default=None,
+    type=click.Path(file_okay=True, dir_okay=False, path_type=Path),
+)
+def cli(by_user_csv: Path, output_csv: Path, by_pi_csv: Path) -> None:
     # Generates CPU averages
-    wide = pd.read_csv(input_csv, dtype_backend="pyarrow")
-    year = int(re.match(r"(\d{4})-", Path(input_csv).name)[1])
+    wide = pd.read_csv(by_user_csv, dtype_backend="pyarrow")
+    year = int(re.match(r"(\d{4})-", Path(by_user_csv).name)[1])  # type: ignore
     long = wide.melt(var_name="username", value_name="cpu_hours")
     long["Year"] = year
 
     u2d = pd.read_csv(
-        MAP_FILE, usecols=["sAMAccountName", "department"], dtype_backend="pyarrow"
-    ).rename(columns={"sAMAccountName": "username", "department": "Dept"})
+        FASRC_MAP_FILE,
+        usecols=["sAMAccountName", "PrimaryGroup", "department"],
+        dtype_backend="pyarrow",
+    ).rename(
+        columns={
+            "sAMAccountName": "username",
+            "PrimaryGroup": "PI",
+            "department": "Dept_orig",
+        }
+    )
 
+    i2d = pd.read_csv(
+        IQSS_MAP_FILE, usecols=["Dept", "PI"], dtype_backend="pyarrow"
+    ).rename(columns={"Dept": "Dept_override"})
+
+    cpu_with_pi = long.merge(u2d, how="inner",on="username")
+    # This will only use the keys we care about
+    cpu_resolved = cpu_with_pi.merge(i2d, on="PI", how="right").assign(
+        Dept=lambda df: df["Dept_override"].fillna(df["Dept_orig"])
+    )
     out = (
-        long.merge(u2d, how="inner")
+        cpu_resolved
         .groupby(["Dept", "Year"], sort=True, as_index=False)
         .agg(Num=("cpu_hours", "sum"))
     )
